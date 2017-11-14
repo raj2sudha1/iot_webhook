f62e602485a971f58850a9eb7a1ce84b24882790
raj2sudha <raj2sudha1@gmail.com>
2017-11-14 12:19:56 +0000

    Updated the listener code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict"
 
+process.title="IOTwebhook";
+
 const express = require('express');
 const GetwebhookHandler = require('github-webhook-handler');
 var webhookHandler = GetwebhookHandler({ path: '/webhook', secret: 'secret' });
 
-let app = express();
+let server = express();
 //app.use(bodyParser.json()); // must use bodyParser in express
-app.use(webhookHandler); // use our middleware
+server.use(webhookHandler); // use our middleware
 
 webhookHandler.on('push', function (repo, data) {
    console.log('Received a push event for %s to %s',
@@ -16,12 +18,54 @@ webhookHandler.on('push', function (repo, data) {
 webhookHandler.on('error', function (err) {
   console.error('Error:', err.message)
 })
-app.get("/", (req, res) =>
+
+server.get("/", (req, res) =>
 {
    res.status(200).send("Hello");
 });
 
-app.on("listening", function () { app.close(); });
-app.listen(3000, () => {
-   console.log("I'm listening.")
+/* handle SIGTERM and SIGINT (ctrl-c) nicely */
+process.once('SIGTERM', end);
+process.once('SIGINT', end);
+
+//app.listen(3000, () => {
+//   console.log("I'm listening.")
+//});
+var listener = server.listen(8000, function(err) {
+    if (err) throw err;
+
+    var host = listener.address().address;
+    var port = listener.address().port;
+
+    console.log('Server listening at http://%s:%s', host, port);
 });
+
+
+var lastSocketKey = 0;
+var socketMap = {};
+listener.on('connection', function(socket) {
+    /* generate a new, unique socket-key */
+    var socketKey = ++lastSocketKey;
+    /* add socket when it is connected */
+    socketMap[socketKey] = socket;
+    socket.on('close', function() {
+        /* remove socket when it is closed */
+        delete socketMap[socketKey];
+    });
+});
+
+function end() {
+    /* loop through all sockets and destroy them */
+    Object.keys(socketMap).forEach(function(socketKey){
+        socketMap[socketKey].destroy();
+    });
+
+    /* after all the sockets are destroyed, we may close the server! */
+    listener.close(function(err){
+        if(err) throw err();
+
+        console.log('Server stopped');
+        /* exit gracefully */
+        process.exit(0);
+    });
+}
