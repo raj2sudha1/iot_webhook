e65c5d9e5682e7483984885769b23a7bf37d2672
Sudhakar <raj2sudha1@gmail.com>
2017-11-14 12:41:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 "use strict"
 
 const express = require('express');
+const webhookHandler = require('github-webhook-handler');
+var webhookHandler = webhookHandler({ path: '/webhook', secret: 'secret' });
+
 let app = express();
+//app.use(bodyParser.json()); // must use bodyParser in express
+app.use(webhookHandler); // use our middleware
+
+webhookHandler.on('push', function (repo, data) {
+   console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+});
 
 app.get("/", (req, res) =>
 {
